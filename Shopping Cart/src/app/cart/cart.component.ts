bb25ec5d3f9f564b78c74c8ef55541099116e430
Sphoenix1708 <sphoenix1708@gmail.com>
2023-03-09 15:38:43 +0530

    First Commit

---
+++ Shopping Cart/src/app/cart/cart.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css'],
+})
+export class CartComponent {
+  items = this.cartService.getItems();
+
+  checkoutForm = this.formBuilder.group({
+    name: ' ',
+    address: ' ',
+  });
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private cartService: CartService
+  ) {}
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been placed', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+}
